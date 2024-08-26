38aa6e75dc8ef4772233abcb8b3e0adea925197b
Sanidhya Uniyal <uniyalths@gmail.com>
2024-08-26 09:32:02 +0530

    Update config.py

--- web_app/app/config.py
+++ web_app/app/config.py
@@ -1,6 +1,9 @@
 # app/config.py
+import os
 
 class Config:
+    # Use the environment variable if set, otherwise default to a secure key
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_generated_secret_key_here')
     HOST = 'localhost'
     USER = 'root'
     PASSWORD = '1234'
