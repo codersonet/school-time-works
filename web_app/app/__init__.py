167b9adff0f43abb1e8bdd39593c5d558084f4ff
Sanidhya Uniyal <uniyalths@gmail.com>
2024-08-26 09:33:18 +0530

    Update __init__.py

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -2,8 +2,10 @@
 
 from flask import Flask
 from .routes import routes
+import config 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config.Config)
     app.register_blueprint(routes)
     return app
