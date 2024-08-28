2c53676b6a8ca9d4a905130958aaa38da3f9786e
Sanidhya Uniyal <uniyalths@gmail.com>
2024-08-28 20:35:36 +0530

    Update __init__.py

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -2,10 +2,17 @@
 
 from flask import Flask
 from .routes import routes
-import .config from Config # Import Config from config
+from .logging import setup_logging  # Import setup_logging from logging
+from .config import Config  # Import Config from config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config) # Use Config to load the settings
+    app.config.from_object(Config)  # Use Config to load the settings
+
+    # Set up logging
+    setup_logging()
+
+    # Register blueprints or routes
     app.register_blueprint(routes)
+    
     return app
