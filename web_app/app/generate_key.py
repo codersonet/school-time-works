f78650ad5d23ed0a6e226c3422c278c2f25327ed
Sanidhya Uniyal <uniyalths@gmail.com>
2024-10-31 14:35:58 +0530

    Delete web_app directory

--- web_app/app/generate_key.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Generate a random 24-byte string and convert it to a hex representation
-secret_key = os.urandom(24).hex()
-
-# Print the secret key with a message
-print(f"Your secret key: {secret_key}")
